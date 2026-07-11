e075572cd936022073d63005296bfb99535d239a
jssinstitute659-blip <jssinstitute659@gmail.com>
2026-07-11 12:25:10 +0530

    Update studentApi.js

--- frontend/src/services/studentApi.js
+++ frontend/src/services/studentApi.js
@@ -1,6 +1,6 @@
 import API from './authApi'
-
 export const getAllStudents = () => API.get('/api/students')
 export const createStudent = (data) => API.post('/api/students', data)
 export const deleteStudent = (id) => API.delete(`/api/students/${id}`)
 export const getMyProfile = () => API.get('/api/students/me')
+export const updateStudentJoiningDate = (id, joiningDate) => API.patch(`/api/students/${id}/joining-date`, { joiningDate })
